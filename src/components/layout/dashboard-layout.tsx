7c7b279b688a5be1628ad811db6b7839388458dc
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-29 15:33:53 +0000

    feat: dashboard layout setup, search & main header ui

---
+++ src/components/layout/dashboard-layout.tsx
@@ -0,0 +1,13 @@
+import MainHeader from "../ui/main-header"
+import Searchbar from "../ui/search-bar"
+
+export const DashboardLayout: React.FC = () => {
+    return (
+        <>
+            <div className="px-6 py-2">
+                <MainHeader />
+                <Searchbar />
+            </div>
+        </>
+    )
+}
