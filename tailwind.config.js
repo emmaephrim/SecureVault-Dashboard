0fccf7a56583b440a8c913e500998d7777d8f927
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-29 12:08:21 +0000

    feat: roboto fonts added, family, size, spacing & weight configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,37 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Roboto", "ui-sans-serif", "system-ui"],
+      },
+      fontSize: {
+        // Primary)
+        "headline-sm": ["24px", { lineHeight: "32px", letterSpacing: "0", fontWeight: "400" }],
+        "title-lg": ["22px", { lineHeight: "28px", letterSpacing: "0", fontWeight: "400" }],
+        "title-md": ["16px", { lineHeight: "24px", letterSpacing: "0.15px", fontWeight: "500" }],
+        "title-sm": ["14px", { lineHeight: "20px", letterSpacing: "0.1px", fontWeight: "500" }],
+        "label-lg": ["14px", { lineHeight: "20px", letterSpacing: "0.1px", fontWeight: "500" }],
+        "label-md": ["12px", { lineHeight: "16px", letterSpacing: "0.5px", fontWeight: "500" }],
+        "label-sm": ["11px", { lineHeight: "16px", letterSpacing: "0.5px", fontWeight: "500" }],
+        "body-lg": ["16px", { lineHeight: "24px", letterSpacing: "0.5px", fontWeight: "400" }],
+        "body-md": ["14px", { lineHeight: "20px", letterSpacing: "0.25px", fontWeight: "400" }],
+        "body-sm": ["12px", { lineHeight: "16px", letterSpacing: "0.4px", fontWeight: "400" }],
+
+        // Emphasis
+        "headline-sm-emph": ["24px", { lineHeight: "32px", letterSpacing: "0", fontWeight: "500" }],
+        "title-lg-emph": ["22px", { lineHeight: "28px", letterSpacing: "0", fontWeight: "500" }],
+        "title-md-emph": ["16px", { lineHeight: "24px", letterSpacing: "0.15px", fontWeight: "700" }],
+        "title-sm-emph": ["14px", { lineHeight: "20px", letterSpacing: "0.1px", fontWeight: "700" }],
+        "label-lg-emph": ["14px", { lineHeight: "20px", letterSpacing: "0.1px", fontWeight: "700" }],
+        "label-md-emph": ["12px", { lineHeight: "16px", letterSpacing: "0.5px", fontWeight: "700" }],
+        "label-sm-emph": ["11px", { lineHeight: "16px", letterSpacing: "0.5px", fontWeight: "700" }],
+        "body-lg-emph": ["16px", { lineHeight: "24px", letterSpacing: "0.5px", fontWeight: "500" }],
+        "body-md-emph": ["14px", { lineHeight: "20px", letterSpacing: "0.25px", fontWeight: "500" }],
+        "body-sm-emph": ["12px", { lineHeight: "16px", letterSpacing: "0.4px", fontWeight: "500" }],
+      },
+
       colors: {
+        // Colors
         brand: {
           primary: "#D0BCFE",
           "on-primary": "#381E72",
