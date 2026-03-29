34a0268611a040c4a1627532a2eb187accfb4ec2
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-29 15:34:21 +0000

    feat: header ui

---
+++ src/components/ui/main-header.tsx
@@ -0,0 +1,17 @@
+import Logo from "../../assets/logo.png";
+
+export default function MainHeader(): React.ReactElement {
+    return (
+        <>
+            <div className="flex items-center">
+                <div>
+                    <img src={Logo} alt="Logo" />
+                </div>
+                <div className="ml-2">
+                    <h1 className="text-brand-on-surface text-headline-sm-emph">Secure Vault</h1>
+                    <span className="text-brand-outline text-body-sm-emph">Enterprise Cloud Security Dashboard</span>
+                </div>
+            </div>
+        </>
+    )
+}
