0e42f4a5bb69f3bbf0ec09f15f428d2eb1e6ff03
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-30 10:37:58 +0000

    fix: return type

--- src/components/ui/down-chevron.tsx
+++ src/components/ui/down-chevron.tsx
@@ -1,6 +1,6 @@
 import { FaChevronDown } from "react-icons/fa";
 
-export default function DownChevron({ color = "text-brand-outline" }: { color?: string }) {
+export default function DownChevron({ color = "text-brand-outline" }: { color?: string }): React.ReactElement {
     return (
         <FaChevronDown className={`shrink-0 text-title-sm ${color}`} />
     )
