6c5ac682341c174fbc7e1eaf95c7c6c15df8ac51
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-30 15:55:29 +0000

    fix: margin left

--- src/components/ui/folder-icon.tsx
+++ src/components/ui/folder-icon.tsx
@@ -1,7 +1,5 @@
 import { FaRegFolder } from "react-icons/fa";
 
-export default function FolderIcon({ color = "text-brand-primary" }: { color?: string, }): React.ReactElement {
-    return (
-        <FaRegFolder className={`shrink-0 text-title-sm ${color}`} />
-    )
+export default function FolderIcon({ color = "text-brand-primary" }: { color?: string }): React.ReactElement {
+  return <FaRegFolder className={`shrink-0 text-title-md ${color} ml-1`} />;
 }
