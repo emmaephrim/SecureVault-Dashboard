7df58eb2254a575f315403fb5bcd497b792cda59
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-30 08:29:37 +0000

    refactor: file-eplorere-panel component to house tree navigation

---
+++ src/components/file-explorer-panel.tsx
@@ -0,0 +1,11 @@
+import { SectionTitle } from "./ui/section-title";
+
+export default function FileExplorerPanel(): React.ReactElement {
+    return (
+        <section className="p-2 md:pl-6 pt-3 w-full overflow-scroll">
+            <SectionTitle>FILE EXPLORER</SectionTitle>
+            {/* Section for recursive tree */}
+
+        </section>
+    )
+}
