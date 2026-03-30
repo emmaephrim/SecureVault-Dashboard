40cb02ca42e5417c0a4ebc8196af3be347262faa
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-30 15:53:06 +0000

    feat: file tree component imported

--- src/components/file-explorer-panel.tsx
+++ src/components/file-explorer-panel.tsx
@@ -1,11 +1,15 @@
 import { SectionTitle } from "./ui/section-title";
+import data from "../../data.json";
+import FileTree from "./ui/file-tree";
+import type { Node } from "../types";
 
 export default function FileExplorerPanel(): React.ReactElement {
     return (
-        <section className="p-2 md:pl-6 pt-3 w-full overflow-scroll">
+        <section className="p-2 md:pl-6 pt-3 w-full overflow-y-auto">
             <SectionTitle>FILE EXPLORER</SectionTitle>
             {/* Section for recursive tree */}
 
+            <FileTree data={data as Node[]} />
         </section>
     )
 }
