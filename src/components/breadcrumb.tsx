8175fad74967be73794e0151bd3ea31e92f29f96
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-30 23:55:33 +0000

    feat & fix: state & node passed to breadcrum

--- src/components/breadcrumb.tsx
+++ src/components/breadcrumb.tsx
@@ -2,11 +2,25 @@ import { MdOutlineHome } from "react-icons/md";
 import BreadcrumbItem from "./ui/breadcrumb-item";
 import RightChevron from "./ui/right-chevron";
 
-export default function Breadcrumb(): React.ReactElement {
-    return (
-        <>
-            <div className="overflow-x-auto flex flex-row mt-4 items-center w-full whitespace-nowrap">
-                <button className="shrink-0 text-brand-outline text-title-md flex items-center flex-row hover:bg-brand-primary-container hover:text-brand-on-primary-container group py-1 px-2">
+export default function Breadcrumb({ path, onNavigate }): React.ReactElement {
+  return (
+    <>
+      <div className="overflow-x-auto flex flex-row mt-4 items-center w-full whitespace-nowrap transition-all duration-200">
+        <button onClick={() => {}} className="shrink-0 text-brand-outline text-title-md flex items-center flex-row hover:bg-brand-primary-container hover:text-brand-on-primary-container group py-1 px-2">
+          <MdOutlineHome className="h-5 w-5 text-title-md text-brand-outline group-hover:text-brand-on-primary-container" />
+          Root
+        </button>
+        {path.map((node, index) => (
+          <div key={node.id} className="flex items-center transition-all duration-200">
+            <RightChevron />
+
+            {node.type !== "file" && <BreadcrumbItem node={node} onNavigate={onNavigate} />}
+
+            {node.type == "file" && <span className="shrink-0 text-brand-primary text-title-md px-2">{node.name}</span>}
+          </div>
+        ))}
+
+        {/* <button className="shrink-0 text-brand-outline text-title-md flex items-center flex-row hover:bg-brand-primary-container hover:text-brand-on-primary-container group py-1 px-2">
                     <MdOutlineHome className="h-5 w-5 text-title-md text-brand-outline group-hover:text-brand-on-primary-container" />
                     Root
                 </button>
@@ -23,8 +37,24 @@ export default function Breadcrumb(): React.ReactElement {
 
                 <span className="shrink-0 text-brand-primary text-title-md px-2">
                     Partner_Contact_Database.xlsx
-                </span>
-            </div>
-        </>
-    )
+                </span> */}
+      </div>
+    </>
+  );
 }
+
+// export default function Breadcrumb({ path, onNavigate }) {
+//   return (
+//     <div className="flex overflow-x-auto mt-4 text-brand-on-surface">
+//       {path.map((node, index) => (
+//         <div key={node.id} className="flex items-center text-brand-on-surface">
+//           <button onClick={() => onNavigate(node.id)} className="text-brand-on-surface">
+//             {node.name}
+//           </button>
+
+//           {index < path.length - 1 && <RightChevron />}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
--- src/components/ui/breadcrumb-item.tsx
+++ src/components/ui/breadcrumb-item.tsx
@@ -1,7 +1,7 @@
-export default function BreadcrumbItem({ item }: { item: string }): React.ReactElement {
-    return (
-        <button className="shrink-0 text-brand-outline text-title-md text-nowrap flex items-center flex-row hover:bg-brand-primary-container hover:text-brand-on-primary-container group py-1 px-2">
-            {item}
-        </button>
-    )
+export default function BreadcrumbItem({ node, onNavigate }): React.ReactElement {
+  return (
+    <button onClick={() => onNavigate(node.id)} className="shrink-0 text-brand-outline text-title-md text-nowrap flex items-center flex-row hover:bg-brand-primary-container hover:text-brand-on-primary-container group py-1 px-2">
+      {node.name}
+    </button>
+  );
 }
