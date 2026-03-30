545e10164185ee3604ed14b2b6ea54134f391774
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-30 10:39:16 +0000

    fix: return type

--- src/components/ui/right-chevron.tsx
+++ src/components/ui/right-chevron.tsx
@@ -1,7 +1,6 @@
 import { FaChevronRight } from "react-icons/fa";
 
-
-export default function RightChevron({ color = 'text-brand-outline' }: { color?: string }) {
+export default function RightChevron({ color = 'text-brand-outline' }: { color?: string }): React.ReactElement {
     return (
         <FaChevronRight className={`shrink-0 text-title-sm ${color}`} />
     )
