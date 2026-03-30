f3ebd0a35944a9e824df7d5394b847918b154586
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-30 20:15:36 +0000

    fix: section width for overflow

--- src/components/layout/dashboard-layout.tsx
+++ src/components/layout/dashboard-layout.tsx
@@ -1,40 +1,38 @@
-import FileExplorerPanel from "../file-explorer-panel"
-import Footer from "../footer"
-import PropertiesPanel from "../properties-panel"
-import Breadcrumb from "../breadcrumb"
-import MainHeader from "../ui/main-header"
-import Searchbar from "../search-bar"
+import FileExplorerPanel from "../file-explorer-panel";
+import Footer from "../footer";
+import PropertiesPanel from "../properties-panel";
+import Breadcrumb from "../breadcrumb";
+import MainHeader from "../ui/main-header";
+import Searchbar from "../search-bar";
 
 export const DashboardLayout: React.FC = () => {
-    return (
-        <>
-            <section className="fixed w-full top-0 bg-brand-surface">
-                <div className="px-2 md:px-6 py-3">
-                    {/* Header with logo */}
-                    <MainHeader />
-                    {/* Search Bar */}
-                    <Searchbar />
-                    {/* Breadcrumb for directory navigation */}
-                    <Breadcrumb />
-                </div>
+  return (
+    <>
+      <section className="fixed w-full top-0 bg-brand-surface">
+        <div className="px-2 md:px-6 py-3">
+          {/* Header with logo */}
+          <MainHeader />
+          {/* Search Bar */}
+          <Searchbar />
+          {/* Breadcrumb for directory navigation */}
+          <Breadcrumb />
+        </div>
 
-                <div>
-                    <hr className="border-t-1 border-brand-primary" />
-                </div>
-            </section>
+        <div>
+          <hr className="border-t-1 border-brand-primary" />
+        </div>
+      </section>
 
+      <section className="mt-[189px] w-full">
+        <div className="flex flex-row justify-end">
+          <FileExplorerPanel />
 
+          <PropertiesPanel />
+        </div>
+      </section>
 
-            <section className="mt-[189px]">
-                <div className="flex flex-row justify-end">
-                    <FileExplorerPanel />
-
-                    <PropertiesPanel />
-                </div>
-            </section>
-
-            {/* footer */}
-            <Footer length={8} fileName="Partner_Contact_Database.xlsx" />
-        </>
-    )
-}
+      {/* footer */}
+      <Footer length={8} fileName="Partner_Contact_Database.xlsx" />
+    </>
+  );
+};
