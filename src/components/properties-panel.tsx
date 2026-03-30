abe4c14bd37d7c131a1735fa79f6925605114f69
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-30 15:53:26 +0000

    feat: layout in progress

--- src/components/properties-panel.tsx
+++ src/components/properties-panel.tsx
@@ -1,14 +1,29 @@
+import PropertiesHeader from "./ui/properties-panel-header";
 import { SectionTitle } from "./ui/section-title";
 
-export default function PropertiesPanel(): React.ReactElement {
-    return (
-        <section className="min-w-[40rem] border-brand-primary border-l-[1px] min-h-[calc(100vh-233px)]">
-            <div className="p-5">
-                <SectionTitle>PROPERTIES</SectionTitle>
-            </div>
-            <hr className="border-t-1 border-brand-primary" />
-            {/* Section for file and folder Properties */}
+// function buildMap(data) {
+//   const map = new Map();
 
-        </section>
-    )
+//   function traverse(nodes) {
+//     for (const node of nodes) {
+//       map.set(node.id, node);
+//       if (node.children) traverse(node.children);
+//     }
+//   }
+
+//   traverse(data);
+//   return map;
+// }
+
+export default function PropertiesPanel(): React.ReactElement {
+  return (
+    <section className="min-w-[40rem] border-brand-primary border-l-[1px] min-h-[calc(100vh-233px)]">
+      <div className="p-5">
+        <SectionTitle>PROPERTIES</SectionTitle>
+      </div>
+      <hr className="border-t-1 border-brand-primary" />
+      {/* Section for file and folder Properties */}
+      <PropertiesHeader name="item name" type="file" />
+    </section>
+  );
 }
