f0eaf1a3477f7d5f9128ee369b7dbf94f3b0a79d
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-30 23:56:24 +0000

    refactor: treeNode and fileTree props added

--- src/types.ts
+++ src/types.ts
@@ -6,7 +6,20 @@ export type Node = {
   children?: Node[];
 };
 
-export type Props = {
+export type FileTreeProps = {
   data: Node[];
   depth?: number;
+  expanded: Set<string>;
+  setExpanded: (expanded: Set<string>) => void;
+  selectedId: string | null;
+  setSelectedId: (id: string | null) => void;
+};
+
+export type TreeNodeProps = {
+  node: Node;
+  depth: number;
+  expanded: Set<string>;
+  setExpanded: (expanded: Set<string>) => void;
+  selectedId: string | null;
+  setSelectedId: (id: string | null) => void;
 };
