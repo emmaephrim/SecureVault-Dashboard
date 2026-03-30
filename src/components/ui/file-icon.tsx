46b12a39f5fc2f233845bb0cee866740dad5fe01
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-30 15:54:48 +0000

    fix: margin lef

--- src/components/ui/file-icon.tsx
+++ src/components/ui/file-icon.tsx
@@ -1,7 +1,5 @@
 import { FaRegFile } from "react-icons/fa";
 
-export default function FileIcon({ color = "text-brand-primary" }: { color?: string, }): React.ReactElement {
-    return (
-        <FaRegFile className={`shrink-0 text-title-sm ${color}`} />
-    )
+export default function FileIcon({ color = "text-brand-primary" }: { color?: string }): React.ReactElement {
+  return <FaRegFile className={`shrink-0 text-title-md ${color} ml-1`} />;
 }
