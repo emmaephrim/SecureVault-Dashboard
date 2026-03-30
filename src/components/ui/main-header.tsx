3c4a779da21062f66487c15392eda36fb5e2d471
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-30 06:20:49 +0000

    fix: margin between logo & text

--- src/components/ui/main-header.tsx
+++ src/components/ui/main-header.tsx
@@ -7,7 +7,7 @@ export default function MainHeader(): React.ReactElement {
                 <div>
                     <img src={Logo} alt="Logo" />
                 </div>
-                <div className="ml-2">
+                <div className="ml-3">
                     <h1 className="text-brand-on-surface text-headline-sm-emph">Secure Vault</h1>
                     <span className="text-brand-outline text-body-sm-emph">Enterprise Cloud Security Dashboard</span>
                 </div>
