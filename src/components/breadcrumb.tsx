e83c6bf23f31c85ca038adb441d85bd170c40b9a
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-31 16:28:36 +0000

    feat: clearing search query stated added to root button

--- src/components/breadcrumb.tsx
+++ src/components/breadcrumb.tsx
@@ -3,13 +3,14 @@ import BreadcrumbItem from "./ui/breadcrumb-item";
 import RightChevron from "./ui/right-chevron";
 import type { Node, SetSelected } from "../types";
 
-export default function Breadcrumb({ path, onNavigate }: { path: Node[]; onNavigate: SetSelected }): React.ReactElement {
+export default function Breadcrumb({ path, onNavigate, setQuery }: { path: Node[]; onNavigate: SetSelected; setQuery: (e: string) => void }): React.ReactElement {
   return (
     <>
       <div className="overflow-x-auto flex flex-row mt-4 items-center w-full whitespace-nowrap transition-all duration-200">
         <button
           onClick={() => {
             onNavigate(null);
+            setQuery("");
           }}
           className="shrink-0 text-brand-outline text-title-md flex items-center flex-row hover:bg-brand-primary-container hover:text-brand-on-primary-container group py-1 px-2 border-brand-inverse-on-surface border-l-2"
         >
