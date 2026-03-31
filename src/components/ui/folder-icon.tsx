0c2efb1130a57ea4caa4dbfcf1b4e360e3fbab48
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-31 13:58:22 +0000

    fix: folder icon changed

--- src/components/ui/folder-icon.tsx
+++ src/components/ui/folder-icon.tsx
@@ -1,4 +1,4 @@
-import { FaRegFolder } from "react-icons/fa";
+import { FaRegFolder } from "react-icons/fa6";
 
 export default function FolderIcon({ color = "text-brand-primary" }: { color?: string }): React.ReactElement {
   return <FaRegFolder className={`shrink-0 text-title-md w-5 h-5 group-hover:text-brand-inverse-surface ${color} ml-2`} />;
