0254837c36c8ff481aec12599a4c06c5e5a0bd12
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-31 16:29:38 +0000

    feat: search query made functional

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,16 +1,21 @@
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch } from "react-icons/fa";
 
-export default function Searchbar(): React.ReactElement {
-    return (
-        <>
-            <div className="relative flex items-center mt-4">
-                <input type="search" placeholder='Search files and folders' className=" text-body-md text-brand-outline px-4 pl-10 border-brand-inverse-primary border-2 rounded-[10px] bg-transparent w-full h-[45px] focus:border-brand-primary focus:outline-none focus:text-brand-on-surface hover:bg-brand-on-secondary focus:bg-transparent" />
+export default function Searchbar({ query, setQuery }: { query: string; setQuery: (e: string) => void }): React.ReactElement {
+  return (
+    <>
+      <div className="relative flex items-center mt-4">
+        <input
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          type="search"
+          placeholder="Search files and folders"
+          className=" text-body-md text-brand-outline px-4 pl-10 border-brand-inverse-primary border-2 rounded-[10px] bg-transparent w-full h-[45px] focus:border-brand-primary focus:outline-none focus:text-brand-on-surface hover:bg-brand-on-secondary focus:bg-transparent"
+        />
 
-                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                    <FaSearch className="h-5 w-5 text-brand-outline" aria-hidden="true" />
-                </div>
-
-            </div>
-        </>
-    )
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+          <FaSearch className="h-5 w-5 text-brand-outline" aria-hidden="true" />
+        </div>
+      </div>
+    </>
+  );
 }
