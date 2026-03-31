5281552056a7c1682dbf08fdafda567352051cdb
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-31 00:30:09 +0000

    feat: empty state component added for properties panel landing screen

--- src/components/properties-panel.tsx
+++ src/components/properties-panel.tsx
@@ -1,14 +1,10 @@
 import type { Node } from "../types";
+import EmptyState from "./ui/empty-state";
 import PropertiesMetadata from "./ui/properties-metadata";
 import PropertiesHeader from "./ui/properties-panel-header";
 import { SectionTitle } from "./ui/section-title";
 
 export default function PropertiesPanel({ node }: { node?: Node | null }): React.ReactElement {
-  if (!node) {
-    // return <EmptyState />
-    return <h1>I'm Empty</h1>;
-  }
-
   return (
     <section className="min-w-[30rem] border-brand-primary border-l-[1px] min-h-[calc(100vh-233px)] overflow-x-auto">
       <div className="p-5">
@@ -16,27 +12,16 @@ export default function PropertiesPanel({ node }: { node?: Node | null }): React
       </div>
       <hr className="border-t-1 border-brand-primary w-full" />
       {/* Section for file and folder Properties */}
-      {/* <PropertiesHeader name="item name" type="folder" />
-      <PropertiesMetadata name="Partner_Contact_Database.xlsx" size={"1KB"} type="Folder" items={2} id={"log_data"} /> */}
 
-      <PropertiesHeader name={node.name} type={node.type} />
+      {!node ? (
+        <EmptyState />
+      ) : (
+        <>
+          <PropertiesHeader name={node.name} type={node.type} />
 
-      <PropertiesMetadata name={node.name} size={node.size} type={node.type} items={node.children?.length || 0} id={node.id} />
+          <PropertiesMetadata name={node.name} size={node.size} type={node.type} items={node.children?.length || 0} id={node.id} />
+        </>
+      )}
     </section>
   );
 }
-
-// export default function PropertiesPanel({ node }) {
-//   if (!node) {
-//     // return <EmptyState />
-//     return <h1>I'm Empty</h1>;
-//   }
-
-//   return (
-//     <>
-//       <PropertiesHeader name={node.name} type={node.type} />
-
-//       <PropertiesMetadata name={node.name} size={node.size} type={node.type} items={node.children?.length || 0} id={node.id} />
-//     </>
-//   );
-// }
---
+++ src/components/ui/empty-state.tsx
@@ -0,0 +1,10 @@
+import { FiHardDrive } from "react-icons/fi";
+
+export default function EmptyState(): React.ReactElement {
+  return (
+    <div className="flex flex-col w-full h-full items-center justify-center">
+      <FiHardDrive className="text-brand-outline h-[4rem] w-[4rem]" />
+      <p className="text-brand-outline text-body-sm-emph">Select file or folder to view details</p>
+    </div>
+  );
+}
