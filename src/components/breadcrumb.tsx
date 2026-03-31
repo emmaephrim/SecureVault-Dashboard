b0b57e02bfc516936a1ed06fe983eb6960746f91
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-31 11:49:35 +0000

    fix: breadcrumb root button fixed

--- src/components/breadcrumb.tsx
+++ src/components/breadcrumb.tsx
@@ -6,7 +6,12 @@ export default function Breadcrumb({ path, onNavigate }): React.ReactElement {
   return (
     <>
       <div className="overflow-x-auto flex flex-row mt-4 items-center w-full whitespace-nowrap transition-all duration-200">
-        <button onClick={() => {}} className="shrink-0 text-brand-outline text-title-md flex items-center flex-row hover:bg-brand-primary-container hover:text-brand-on-primary-container group py-1 px-2">
+        <button
+          onClick={() => {
+            onNavigate(null);
+          }}
+          className="shrink-0 text-brand-outline text-title-md flex items-center flex-row hover:bg-brand-primary-container hover:text-brand-on-primary-container group py-1 px-2"
+        >
           <MdOutlineHome className="h-5 w-5 text-title-md text-brand-outline group-hover:text-brand-on-primary-container" />
           Root
         </button>
