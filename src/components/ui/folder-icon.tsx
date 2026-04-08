6e8e42d0d62e14e548300e640cf97aa100ea9f64
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-04-08 13:07:28 +0000

    feat: folder icon changed upon open

--- src/components/ui/folder-icon.tsx
+++ src/components/ui/folder-icon.tsx
@@ -1,5 +1,5 @@
 import { FaRegFolder } from "react-icons/fa6";
-import { FaRegFolderOpen } from "react-icons/fa";
+import { FaRegFolderOpen } from "react-icons/fa6";
 
 export default function FolderIcon({ color = "text-brand-primary", isExpanded }: { color?: string; isExpanded: boolean }): React.ReactElement {
   return isExpanded ? <FaRegFolderOpen className={`shrink-0 text-title-md w-5 h-5 group-hover:text-brand-inverse-surface ${color} ml-2`} /> : <FaRegFolder className={`shrink-0 text-title-md w-5 h-5 group-hover:text-brand-inverse-surface ${color} ml-2`} />;
